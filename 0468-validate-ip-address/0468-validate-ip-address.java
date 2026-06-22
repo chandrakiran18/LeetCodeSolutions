cfc0f82ded163aebf7f49f0182a8fc679b09c307
chandrakiran18 <chandrakiranbobbadi@gmail.com>
2026-06-22 14:37:23 +0530

    Time: 2 ms (34.99%), Space: 42.8 MB (75.36%) - LeetHub

--- 0468-validate-ip-address/0468-validate-ip-address.java
+++ 0468-validate-ip-address/0468-validate-ip-address.java
@@ -1,64 +1,41 @@
 import java.util.StringTokenizer;
-
 class Solution {
-
-    public static boolean isValid(String s) {
-        String[] parts = s.split(":", -1);
-        if (parts.length != 8) return false;
-        for (String part : parts) {
-            if (part.isEmpty() || part.length() > 4) return false;
-            for (char c : part.toCharArray()) {
-                if (!((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F')))
-                    return false;
-            }
-        }
-        return true;
-    }
-
     public String validIPAddress(String queryIP) {
-        String ans = "Neither";
-
-        if (queryIP.indexOf('.') != -1 && queryIP.indexOf(':') != -1)
-            return ans;
-
-        if (queryIP.indexOf('.') != -1) {
-            if (queryIP.charAt(0) == '.' || queryIP.charAt(queryIP.length() - 1) == '.')
-                return ans;
-
-            if (queryIP.contains(".."))
-                return ans;
-
-            StringTokenizer st = new StringTokenizer(queryIP, ".");
-            if (st.countTokens() != 4) return ans;
-
-            while (st.hasMoreTokens()) {
-                String temp1 = st.nextToken();
-
-                if (temp1.length() == 0 || temp1.length() > 3) return ans;
-
-                for (int i = 0; i < temp1.length(); i++) {
-                    char c = temp1.charAt(i);
-                    if (c < '0' || c > '9') return ans;
+        String[] parts = queryIP.split("[.:]", -1);
+        String ans= "Neither";
+        if(parts.length==4){
+            StringTokenizer st=new StringTokenizer(queryIP,".");
+            if(st.countTokens()!=4)return ans;
+            if(queryIP.charAt(0)=='.'|| queryIP.charAt(queryIP.length()-1)=='.')return ans;
+            for(String s:parts){
+                if(s.length()==0 || s.length()>3)return ans;
+                if(s.charAt(0)=='0' && s.length()>1)return ans;
+                for(char c:s.toCharArray()){
+                    if(c>='a' && c<='z')return ans;
+                    if(c>='A' && c<='Z')return ans;
+                    if(c==':')return ans;
                 }
-
-                if (temp1.charAt(0) == '0' && temp1.length() > 1) return ans;
-
-                int temp = Integer.parseInt(temp1);
-                if (temp < 0 || temp > 255) return ans;
+                int temp=Integer.parseInt(s);
+                if(temp>=0 && temp<=255)continue;
+                else return  ans;
             }
-
-            ans = "IPv4";
+            ans="IPv4";
             return ans;
-        } else if (queryIP.indexOf(':') != -1) {
-            if (queryIP.charAt(0) == ':' || queryIP.charAt(queryIP.length() - 1) == ':')
-                return ans;
-
-            if (isValid(queryIP)) {
-                ans = "IPv6";
-                return ans;
-            } else return ans;
+        }else if(parts.length==8){
+            StringTokenizer st=new StringTokenizer(queryIP,":");
+            if(st.countTokens()!=8)return ans;
+            if(queryIP.charAt(0)==':'|| queryIP.charAt(queryIP.length()-1)==':')return ans;
+            for(String s:parts){
+                if(s.length()==0 ||s.length()>4)return ans;
+                for(char c:s.toCharArray()){
+                    if(!((c>='0' && c<='9') || (c>='a' && c<='f') || (c>='A' && c<='F'))){
+                        return ans;
+                    }
+                }                
+            }
+            ans="IPv6";
+            return ans;
         }
-
         return ans;
     }
 }

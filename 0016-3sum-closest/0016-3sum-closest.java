c0363fc699c5ee610ad7e4b19c64042bd28e7ac5
chandrakiran18 <chandrakiranbobbadi@gmail.com>
2026-06-23 15:52:54 +0530

    Time: 662 ms (5.01%), Space: 45 MB (95.19%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -7,7 +7,7 @@ class Solution {
             for(int j=i+1;j<n-1;j++){
                 for(int k=j+1;k<n;k++){
                     int sum=nums[i]+nums[j]+nums[k];
-                    // if(sum==target)continue;
+                    if(sum==target)return target;
                     int currdiff;
                     if(sum>target){
                         currdiff=sum-target;

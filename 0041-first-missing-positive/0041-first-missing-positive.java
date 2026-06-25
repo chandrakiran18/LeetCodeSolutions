e18d45ffffb28f40e6b5b00179529fc06f0d7817
chandrakiran18 <chandrakiranbobbadi@gmail.com>
2026-06-25 14:31:25 +0530

    Time: 18 ms (11.97%), Space: 73.8 MB (27.72%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -8,11 +8,11 @@ class Solution {
             while(i<n-1 && nums[i]==nums[i+1]){
                 i++;
             }
-            if(ans!=nums[i]){
-                if(ans>nums[i])continue;
-                else return ans;
+            if(nums[i]==ans){
+                ans++;
+            }else if(nums[i]>ans){
+                return ans;
             }
-            ans+=1;
         }
         return ans;
     }

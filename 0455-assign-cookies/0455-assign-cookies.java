36c792d4dd3641ec0afce9c727420a129931b23c
chandrakiran18 <chandrakiranbobbadi@gmail.com>
2026-06-25 16:03:00 +0530

    Time: 14 ms (93.39%), Space: 51.4 MB (73.91%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -10,7 +10,7 @@ class Solution {
                 j++;
                 i++;
                 count++;
-            }else if(s[j]<g[i]){
+            }else{
                 j++;
             }
         }

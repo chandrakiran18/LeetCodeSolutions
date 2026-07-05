915237558d2137018bb90327171d7fe04d06f51f
chandrakiran18 <chandrakiranbobbadi@gmail.com>
2026-07-05 19:51:58 +0530

    Time: 0 ms (100%), Space: 43.1 MB (11.4%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.java
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.java
@@ -1,9 +1,8 @@
 class Solution {
     public boolean checkRecord(String s) {
-        char[] record=s.toCharArray();
         int abs=0;
         int late=0;
-        for(char c:record){
+        for(char c:s.toCharArray()){
             if(c=='A'){
                 abs++;
                 if(abs>=2)return false;

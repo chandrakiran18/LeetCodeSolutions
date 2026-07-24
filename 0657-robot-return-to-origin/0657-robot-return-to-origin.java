3a7885570db8aa6bd656036b93b05f754197bcb9
chandrakiran18 <chandrakiranbobbadi@gmail.com>
2026-07-24 23:30:51 +0530

    Time: 5 ms (69.01%), Space: 46 MB (45.75%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x=0;
+        int y=0;
+        for(char c:moves.toCharArray()){
+            if(c=='U')x++;
+            else if(c=='D')x--;
+            else if(c=='R')y++;
+            else y--;
+        }
+        if(x==0 && y==0)return true;
+        return false;
+    }
+}

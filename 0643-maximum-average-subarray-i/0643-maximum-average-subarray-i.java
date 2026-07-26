220039dbbeb4fa71d8d78a4a8b15a1149d2bc184
chandrakiran18 <chandrakiranbobbadi@gmail.com>
2026-07-26 13:57:10 +0530

    Time: 5 ms (31.78%), Space: 67.3 MB (99.28%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -3,14 +3,11 @@ class Solution {
         int n=nums.length;
         int[] prefix=new int[n+1];
         double ans=Integer.MIN_VALUE;
-        int max=0;
-        prefix[0]=0;
         for(int i=0;i<n;i++){
             prefix[i+1]=prefix[i]+nums[i];
         }
         for(int i=0;i<=n-k;i++){
-            int j=i+k;
-            max=prefix[j]-prefix[i];
+            int max=prefix[i+k]-prefix[i];
             ans=Math.max(ans,max);
         }
         return ans/k;

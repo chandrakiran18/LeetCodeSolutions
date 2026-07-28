086ded150fdf086849a4ac5909e3c51b29393f90
chandrakiran18 <chandrakiranbobbadi@gmail.com>
2026-07-28 09:51:52 +0530

    Time: 4 ms (87.93%), Space: 47.2 MB (18.65%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -3,9 +3,8 @@ class Solution {
         String[] parts=s.split(" ");
         StringBuilder ans=new StringBuilder();
         for(int i=0;i<parts.length;i++){
-            StringBuilder sb=new StringBuilder(parts[i]);
-            ans.append(sb.reverse());
-            if(i!=parts.length-1){
+            ans.append(new StringBuilder(parts[i]).reverse());
+            if(i<parts.length-1){
                 ans.append(" ");
             }
         }

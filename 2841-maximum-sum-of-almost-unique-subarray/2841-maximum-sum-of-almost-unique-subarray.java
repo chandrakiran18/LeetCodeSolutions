d0dd3d87c086447bd37e13d909911af85e1354a3
chandrakiran18 <chandrakiranbobbadi@gmail.com>
2026-08-21 23:35:23 +0530

    Time: 27 ms (25.65%), Space: 51.7 MB (29.74%) - LeetHub

---
+++ 2841-maximum-sum-of-almost-unique-subarray/2841-maximum-sum-of-almost-unique-subarray.java
@@ -0,0 +1,38 @@
+class Solution {
+    public long maxSum(List<Integer> nums, int m, int k) {
+        long ans=0;
+        int n=nums.size();
+        long sum=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<k;i++){
+            sum+=nums.get(i);
+            if(!map.containsKey(nums.get(i))){
+                map.put(nums.get(i),map.getOrDefault(nums.get(i),0)+1);
+            }else{
+                map.put(nums.get(i),map.getOrDefault(nums.get(i),0)+1);
+            }
+        }
+        if(map.size()>=m){
+            ans=sum;
+        }
+        for(int i=k;i<n;i++){
+            int x=nums.get(i);
+            int y=nums.get(i-k);
+            sum+=x;
+            sum-=y;
+            if(!map.containsKey(x)){
+                map.put(x,map.getOrDefault(x,0)+1);
+            }else{
+                map.put(x,map.getOrDefault(x,0)+1);
+            }
+            map.put(y,map.getOrDefault(y,0)-1);
+            if(map.get(y)==0){
+                map.remove(y);
+            }
+            if(map.size()>=m){
+                ans=Math.max(ans,sum);
+            }
+        }
+        return ans;
+    }
+}
